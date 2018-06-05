74e7cc29b4b73ec49dd77646ac83946d27e30810
Aluno <Aluno>
2018-06-05 17:23:08 -0300

    Versão 1

---
+++ src/main/java/br/com/db1/start/classes/Cidade.java
@@ -0,0 +1,51 @@
+package br.com.db1.start.classes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import br.com.db1.start.tipos.Uf;
+
+@Entity(name = "cidade")
+public class Cidade {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+
+	@Column(nullable = false, length = 50, name = "nome")
+	private String nome;
+
+	@Column(name = "uf", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Uf uf;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Uf getUf() {
+		return uf;
+	}
+
+	public void setUf(Uf uf) {
+		this.uf = uf;
+	}
+
+}
